d094643a2677cee643195be6f7644778cd5580ec
John Wick <johnwickiscoding@gmail.com>
2024-09-20 19:51:29 +0530

    Create playRound function
    playRound function takes the human and computer player choices as
    arguments, plays a single round, increments the round winner’s score
    and logs a winner announcement

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let max = 4
     let min = 1
@@ -16,4 +19,30 @@ function getComputerChoice() {
 function getHumanChoice() {
     let userChoice = prompt("Make a choice!! rock, paper or scissors?", "rock")
     return userChoice
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock")
+        computerScore++
+    }else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper")
+        computerScore++
+    }else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors")
+        computerScore++
+    }else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors")
+        humanScore++
+    }else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock")
+        humanScore++
+    }else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper")
+        humanScore++
+    }else {
+        console.log("Draw")
+    }
 }
